6a0dccdb3831ec7afb4004db22da74ef2490c898
Binh Tran <tranhuubinh223@gmail.com>
2022-01-12 05:23:54 +0700

    refactoring

--- CardMatchingServer/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.project.cardmatchingserver {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.project.cardmatchingserver to javafx.fxml;
-    exports com.project.cardmatchingserver;
-}
